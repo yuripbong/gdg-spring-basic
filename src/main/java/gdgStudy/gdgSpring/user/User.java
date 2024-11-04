10a1c48a93ab9e36bd2ed2de1ca2ec6e16df77b5
yuripbong <kyri200350@gmail.com>
2024-11-04 21:05:51 +0900

    refactor: 테이블 매핑

--- src/main/java/gdgStudy/gdgSpring/user/User.java
+++ src/main/java/gdgStudy/gdgSpring/user/User.java
@@ -8,14 +8,20 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String email;
 
     @Builder
