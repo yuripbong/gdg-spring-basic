a63b94169f769b478dd9cb9c993dd7b81a03ad64
yuripbong <kyri200350@gmail.com>
2024-11-04 20:18:09 +0900

    feat: 회원 컨트롤러 CRUD 구성

--- src/main/java/gdgStudy/gdgSpring/user/UserController.java
+++ src/main/java/gdgStudy/gdgSpring/user/UserController.java
@@ -3,22 +3,72 @@ package gdgStudy.gdgSpring.user;
 import gdgStudy.gdgSpring.user.dto.request.UserSaveRequestDto;
 import gdgStudy.gdgSpring.user.dto.response.UserSaveResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
+    @Autowired
     private UserService userService;
 
     // CREATE (생성)
-    @PostMapping("/users")
-    public ResponseEntity<UserSaveResponseDto> createUser(@RequestBody UserSaveRequestDto userDto) {
-        UserSaveResponseDto createdUser = userService.createUser(userDto);
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserSaveRequestDto userSaveRequestDto) {
+        User createdUser = userService.createUser(userSaveRequestDto);
         return ResponseEntity.ok(createdUser);
     }
 
-    // READ
+    // READ (조회)
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
 
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
+        User user = (User) userService.getUserById(id);
+
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return ResponseEntity.ok(user);
+        }
+    }
+
+    @GetMapping("/byusername/{username}")
+    public ResponseEntity<List<User>> getUserByUsername(@PathVariable("username") String username) {
+        List<User> users = userService.getUsersByUsername(username);
+
+        return ResponseEntity.ok(users);
+    }
+
+    // UPDATE (수정)
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") Long id, @RequestBody UserSaveRequestDto updateUserRequestDto) {
+        User user = userService.updateUser(id, updateUserRequestDto);
+
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return ResponseEntity.ok(user);
+        }
+    }
+
+    // DELETE (삭제)
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable("id") Long id) {
+        boolean deleted = userService.deleteUser(id);
+
+        if (deleted) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
