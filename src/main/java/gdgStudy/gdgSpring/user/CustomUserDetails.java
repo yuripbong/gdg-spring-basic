0e72470aa621571251ac3e4a3c892240844701e0
yuripbong <kyri200350@gmail.com>
2024-12-15 18:07:19 +0900

    feat: username, password 얻어옴

--- src/main/java/gdgStudy/gdgSpring/user/CustomUserDetails.java
+++ src/main/java/gdgStudy/gdgSpring/user/CustomUserDetails.java
@@ -49,4 +49,13 @@ public class CustomUserDetails implements UserDetails {
         return collection;
     }
 
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
 }
