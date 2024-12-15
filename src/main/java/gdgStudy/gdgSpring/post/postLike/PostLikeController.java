856b47db48c9b939f5367e56be94266b09cc6162
yuripbong <kyri200350@gmail.com>
2024-12-15 18:08:05 +0900

    feat: 좋아요 controller 구성

--- src/main/java/gdgStudy/gdgSpring/post/postLike/PostLikeController.java
+++ src/main/java/gdgStudy/gdgSpring/post/postLike/PostLikeController.java
@@ -1,13 +1,33 @@
 package gdgStudy.gdgSpring.post.postLike;
 
+import gdgStudy.gdgSpring.post.postLike.dto.PostLikeRequestDto;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/like")
+@RequestMapping("/postLikes")
 public class PostLikeController {
 
-    private final PostLike
+    private final PostLikeService postLikeService;
+
+    public PostLikeController(PostLikeService postLikeService) {
+        this.postLikeService = postLikeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> insert(@RequestBody @Validated PostLikeRequestDto postLikeRequestDto) {
+        postLikeService.insert(postLikeRequestDto);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> delete(@RequestBody @Validated PostLikeRequestDto postLikeRequestDto) {
+        postLikeService.delete(postLikeRequestDto);
+
+        return ResponseEntity.ok().build();
+    }
 }
