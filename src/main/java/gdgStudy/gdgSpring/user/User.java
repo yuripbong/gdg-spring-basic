104572dfda010a1586c090f466a6a3b1a94e4f61
yuripbong <kyri200350@gmail.com>
2024-12-23 17:35:41 +0900

    feat: 팔로우 필드 추가

--- src/main/java/gdgStudy/gdgSpring/user/User.java
+++ src/main/java/gdgStudy/gdgSpring/user/User.java
@@ -1,12 +1,16 @@
 package gdgStudy.gdgSpring.user;
 
+import gdgStudy.gdgSpring.common.BaseTimeEntity;
+import gdgStudy.gdgSpring.follow.Follow;
 import gdgStudy.gdgSpring.user.dto.request.UserSaveRequestDto;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
 @Getter
 @Entity(name = "users")
-public class User {
+public class User extends BaseTimeEntity {
 
     // NoArgsConstructor 없애고 만든 기본 생성자
     public User() {}
@@ -23,6 +27,13 @@ public class User {
 
     @Column(nullable = false)
     private String email;
+
+    // 팔로우
+    @OneToMany(mappedBy = "from_user")
+    private List<Follow> followings;
+
+    @OneToMany(mappedBy = "to_user")
+    private List<Follow> followers;
 
     public User(UserSaveRequestDto userSaveRequestDto) {
         this.username = userSaveRequestDto.getUsername();
